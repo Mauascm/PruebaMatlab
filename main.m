7659428267b2b9e88f0e977fa875bb8c653885a8
Mauascm <mauricioivanascenciom@gmail.com>
2023-05-27 17:32:15 -0600

    main uptdated

--- main.m
+++ main.m
@@ -9,3 +9,27 @@ for i = 1:20
     subplot(4,5,i);
     imshow(imds.Files{perm(i)});
 end
+
+
+numTrainFiles = 750;
+[imdsTrain,imdsTest] = splitEachLabel(imds,numTrainFiles,'randomize');
+
+layers = [
+    imageInputLayer([28 28 1])
+    convolution2dLayer(5,20)
+    reluLayer
+    maxPooling2dLayer(2,'Stride',2)
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+options = trainingOptions('sgdm', ...
+        'MaxEpochs',20, ...
+        'InitialLearnRate',0.0001, ...
+        'Verbose',false, ...
+        'Plots','training-progress');
+
+net = trainNetwork(imdsTrain,layers,options);
+
+YPred = classify(net,imdsTest);
+accuracy = sum(YPred == imdsTest.Labels)/numel(imdsTest.Labels)
